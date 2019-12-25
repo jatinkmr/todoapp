cc2ab59cbef5cadb936744b4399ef3e025d81516
Jatin Kumar <tech3@entrancezone.com>
2019-12-25 20:54:24 +0530

    database creation Ongoing

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const ports = require('./port.js');
+const db = require("./db");
+
+const collection = "todo";
+
+app.use(app.use(bodyParser.json()));
 
 app.listen(ports.port, () => {
     console.log(`Server started at ${ports.port}`);
