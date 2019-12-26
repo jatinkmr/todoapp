b6b3f1f5032a5e2acbaa94db5a5ebc75d80c78cf
Jatin Kumar <tech3@entrancezone.com>
2019-12-26 14:35:01 +0530

    delete Request

--- server.js
+++ server.js
@@ -52,6 +52,18 @@ app.post("/", (req, res) => {
     });
 });
 
+app.delete("/:id", (req, res) => {
+    const todoID = req.params.id;
+
+    db.getDB().collection(collection).findOneAndDelete({_id: db.getPrimaryKey(todoID)}, (err, result) => {
+        if(err) {
+            console.log(err);
+        } else {
+            res.json(result);
+        }
+    });
+});
+
 db.connect((err) => {
     if(err) {
         console.log("Unable to connect with DataBase");
