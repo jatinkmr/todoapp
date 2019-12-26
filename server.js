a75dba5c1b6e9bf22fa42ad7a708285057cd1f90
Jatin Kumar <tech3@entrancezone.com>
2019-12-26 13:56:50 +0530

    get method

--- server.js
+++ server.js
@@ -3,10 +3,27 @@ const app = express();
 const bodyParser = require("body-parser");
 const ports = require('./port.js');
 const db = require("./db");
+const path = require('path');
 
 const collection = "todo";
 
 app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get("/getTodos", (req, res) => {
+    db.getDB().collection(collection).find({}).toArray((err, documents) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(documents);
+            res.json(documents);
+        }
+    });
+});
+
 
 db.connect((err) => {
     if(err) {
@@ -14,7 +31,7 @@ db.connect((err) => {
         process.exit(1);
     } else {
         app.listen(ports.port, () => {
-            console.log(`DataBase Connecgted and Server started at ${ports.port}`);        
+            console.log(`DataBase Connected and Server started at ${ports.port}`);        
         })
     }    
 });
